30bb9ce33ccd1493d0e63a0181e4695d159977ef
toby-adedipe <toby_adedipe@yahoo.com>
2018-07-02 17:50:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      speed:10
+    }
+  }
   render() {
     return (
       <div className="App">
-        <h1>hello world</h1>
+        <h1>10</h1>
       </div>
     );
   }
